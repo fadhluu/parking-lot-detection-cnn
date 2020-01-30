bb54bf39ab8e4e5d4844e63f63cda13754df6f9e
fadhlu <fadhlurahman.i@gmail.com>
2020-01-30 14:19:23 +0700

    add car object and getter car

--- parking_slot.py
+++ parking_slot.py
@@ -1,10 +1,14 @@
+from car import car
+
+
 class parking_slot():
     def __init__(self, pos_x, pos_y, number):
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.number = 0
+        self.number = number
         self.status = False  # True = occupied, False = empty
         self.car_id = 0
+        self.car = None
 
     def assign_spot(self, car_list):
         for car in car_list:
@@ -12,14 +16,24 @@ class parking_slot():
                           (car.pos_y[1] - car.pos_y[0]) + car.pos_y[0]]
             if car_center[0] in range(self.pos_x[0], self.pos_x[1]) and car_center[1] in range(self.pos_y[0], self.pos_y[1]):
                 self.status = True
-                self.set_car_id(car.number)
+                self.set_car(car)
 
     def scan_spot(self, car_list):
         for car in car_list:
             car_center = [(car.pos_x[1] - car.pos_x[0]) + car.pos_x[0],
                           (car.pos_y[1] - car.pos_y[0]) + car.pos_y[0]]
-            if car_center[0] in range(self.pos_x[0], self.pos_x[1]) and car_center[1] in range(self.pos_y[0], self.pos_y[1]):
+            if car_center[0] not in range(self.pos_x[0], self.pos_x[1]) and car_center[1] not in range(self.pos_y[0], self.pos_y[1]):
                 self.status = False
+                self.set_car(None)
 
-    def set_car_id(self, car_id):
-        self.car_id = car_id
+    def set_car(self, car):
+        self.car = car
+
+    def get_status(self):
+        if self.status:
+            return '| Occupied |'
+        else:
+            return '| Empty |'
+
+    def get_pos_x_and_y(self):
+        return [self.pos_x[0], self.pos_y[1]]
