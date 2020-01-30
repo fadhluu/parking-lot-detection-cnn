5503b044ba37796908fef36e58395c02be8f20bd
fadhlu <fadhlurahman.i@gmail.com>
2020-01-30 12:15:44 +0700

    method for getting car center

--- car.py
+++ car.py
@@ -1,5 +1,10 @@
 class car():
     def __init__(self, number, pos_x, pos_y):
         self.number = number
-        self.pos_x = [0, 0]
-        self.pos_y = [0, 0]
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def get_car_center(self):
+        cc = [(self.pos_x[1] - self.pos_x[0]) + self.pos_x[0],
+              (self.pos_y[1] - self.pos_y[0]) + self.pos_y[0]]
+        return cc
